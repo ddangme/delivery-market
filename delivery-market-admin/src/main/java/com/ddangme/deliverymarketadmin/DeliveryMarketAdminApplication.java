3ba42dbcd18656b17199650b40f33d97c355431a
ddangme <ssilver121@naver.com>
2024-04-19 14:34:36 +0900

    chore: admin 프로젝트 추가

---
+++ delivery-market-admin/src/main/java/com/ddangme/deliverymarketadmin/DeliveryMarketAdminApplication.java
@@ -0,0 +1,13 @@
+package com.ddangme.deliverymarketadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryMarketAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeliveryMarketAdminApplication.class, args);
+    }
+
+}
---
+++ delivery-market-admin/src/test/java/com/ddangme/deliverymarketadmin/DeliveryMarketAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ddangme.deliverymarketadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryMarketAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
