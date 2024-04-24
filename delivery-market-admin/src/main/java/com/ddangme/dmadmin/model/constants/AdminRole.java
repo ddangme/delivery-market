f8010e20dfd9bc83e3a063ab1a5d801591083155
ddangme <ssilver121@naver.com>
2024-04-25 01:57:13 +0900

    feat: admin entity 추가

---
+++ delivery-market-admin/src/main/java/com/ddangme/dmadmin/model/constants/AdminRole.java
@@ -0,0 +1,16 @@
+package com.ddangme.dmadmin.model.constants;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum AdminRole {
+    SUPER_ADMIN("최고 관리자"),
+    ADMIN("일반 관리자"),
+    ;
+
+    private String role;
+
+
+}
