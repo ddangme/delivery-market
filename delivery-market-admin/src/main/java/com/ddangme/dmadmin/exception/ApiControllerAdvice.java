d3771232a79d1f4fcdc416274b87d7690d8e9f96
ddangme <ssilver121@naver.com>
2024-04-26 07:50:51 +0900

    feat: api exception advice 추가

---
+++ delivery-market-admin/src/main/java/com/ddangme/dmadmin/exception/ApiControllerAdvice.java
@@ -0,0 +1,28 @@
+package com.ddangme.dmadmin.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class ApiControllerAdvice {
+
+    @ExceptionHandler(DMAdminException.class)
+    public ResponseEntity<?> badRequestHandler(DMAdminException exception) {
+        log.error("Error occurs", exception);
+
+        return ResponseEntity.status(exception.getErrorCode().getStatus())
+                .body(exception.getErrorCode().getMessage());
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<?> exceptionHandler(RuntimeException exception) {
+        log.error("Error occurs", exception);
+
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(ErrorCode.INTERNAL_SERVER_ERROR.name());
+    }
+}
