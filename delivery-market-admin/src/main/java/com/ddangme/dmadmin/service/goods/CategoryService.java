85d74997e2f1ca86f8c0761486d1674773e12fd4
ddangme <ssilver121@naver.com>
2024-04-26 05:31:37 +0900

    feat: 카테고리 생성 비즈니스 로직 추가

--- delivery-market-admin/src/main/java/com/ddangme/dmadmin/service/goods/CategoryService.java
+++ delivery-market-admin/src/main/java/com/ddangme/dmadmin/service/goods/CategoryService.java
@@ -1,6 +1,7 @@
 package com.ddangme.dmadmin.service.goods;
 
 import com.ddangme.dmadmin.dto.goods.CategoryDTO;
+import com.ddangme.dmadmin.model.goods.Category;
 import com.ddangme.dmadmin.repository.goods.CategoryRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -15,8 +16,16 @@ public class CategoryService {
 
     private final CategoryRepository categoryRepository;
 
+    @Transactional
     public void save(CategoryDTO dto) {
+        Category category = dto.toEntity();
 
+        if (dto.getParentId() != null) {
+            categoryRepository.findById(dto.getParentId())
+                    .orElseThrow();
+        }
+
+        categoryRepository.save(category);
     }
 
 }
