7c9b6f08a7707d08238eded88d8631e5f5f40174
ddangme <ssilver121@naver.com>
2024-04-27 01:32:32 +0900

    test: 카테고리 삭제 테스트 코드

--- delivery-market-admin/src/test/java/com/ddangme/dmadmin/service/goods/CategoryServiceTest.java
+++ delivery-market-admin/src/test/java/com/ddangme/dmadmin/service/goods/CategoryServiceTest.java
@@ -1,9 +1,12 @@
 package com.ddangme.dmadmin.service.goods;
 
+import com.ddangme.dmadmin.dto.AdminDTO;
 import com.ddangme.dmadmin.dto.goods.CategoryDTO;
 import com.ddangme.dmadmin.exception.DMAdminException;
 import com.ddangme.dmadmin.exception.ErrorCode;
+import com.ddangme.dmadmin.model.Admin;
 import com.ddangme.dmadmin.model.goods.Category;
+import com.ddangme.dmadmin.repository.AdminRepository;
 import com.ddangme.dmadmin.repository.goods.CategoryRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -13,6 +16,8 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
@@ -32,13 +37,16 @@ class CategoryServiceTest {
     @Mock
     private CategoryRepository categoryRepository;
 
+    @Mock
+    private AdminRepository adminRepository;
+
     @Test
     void 부모가_있는_카테고리_등록() {
         // Given
         Long parentId = 1L;
 
-        Category parentCategory = createdCategory(parentId, null);
-        CategoryDTO dto = createdCategoryDTO(2L, parentId);
+        Category parentCategory = createCategory(parentId, null);
+        CategoryDTO dto = createCategoryDTO(2L, parentId);
 
         given(categoryRepository.findById(parentId)).willReturn(Optional.of(parentCategory));
 
@@ -54,9 +62,9 @@ class CategoryServiceTest {
     @Test
     void 부모가_없는_카테고리_등록() {
         // Given
-        CategoryDTO dto = createdCategoryDTO(1L, null);
+        CategoryDTO dto = createCategoryDTO(1L, null);
         given(categoryRepository.save(any(Category.class)))
-                .willReturn(createdCategory(1L, null));
+                .willReturn(createCategory(1L, null));
 
         // When
         categoryService.save(dto);
@@ -69,7 +77,7 @@ class CategoryServiceTest {
     void 이미_있는_카테고리_이름으로_등록_시도_시_에러() {
         // Given
         CategoryDTO dto = new CategoryDTO("이미 있는 이름", null);
-        given(categoryRepository.findByName("이미 있는 이름")).willReturn(Optional.of(createdCategory(1L, null)));
+        given(categoryRepository.findByName("이미 있는 이름")).willReturn(Optional.of(createCategory(1L, null)));
 
         DMAdminException exception = assertThrows(DMAdminException.class, () -> {
             categoryService.save(dto);
@@ -141,23 +149,88 @@ class CategoryServiceTest {
         assertThat(exception.getErrorCode()).isEqualTo(ErrorCode.UNABLE_LENGTH_CATEGORY_NAME);
     }
 
+    @Test
+    void 선택한_카테고리가_없는_경우_삭제_시도_시_에러() {
+        List<Long> categoryIds = new ArrayList<>();
 
-    private CategoryDTO createdCategoryDTO(Long id, Long parentId) {
+        AdminDTO adminDTO = createAdminDTO();
+
+        DMAdminException exception = assertThrows(DMAdminException.class, () -> {
+            categoryService.delete(categoryIds, adminDTO);
+        });
+
+        assertThat(exception.getErrorCode()).isEqualTo(ErrorCode.NOT_CHOICE_CATEGORY);
+    }
+
+    @Test
+    void 하위_카테고리가_있는_상위_카테고리_삭제_시도_시_에러() {
+        Long parentId = 1L;
+        Category childCategory = createCategory(2L, parentId);
+        Category parentCategory = createCategoryWithChild(parentId, Set.of(childCategory));
+
+        List<Long> categoryIds = List.of(parentId);
+        AdminDTO adminDTO = createAdminDTO();
+
+        given(adminRepository.findById(adminDTO.getId())).willReturn(Optional.of(adminDTO.toEntity()));
+        given(categoryRepository.findById(parentId)).willReturn(Optional.of(parentCategory));
+
+        DMAdminException exception = assertThrows(DMAdminException.class, () -> {
+            categoryService.delete(categoryIds, adminDTO);
+        });
+
+        assertThat(exception.getErrorCode()).isEqualTo(ErrorCode.EXIST_CHILD_CATEGORY);
+    }
+
+    @Test
+    void 카테고리_삭제_DELETE_AT과_DELETE_BY에_데이터_입력() {
+        List<Long> categoryIds = List.of(1L);
+
+        Category category = createCategory(1L, null);
+
+        AdminDTO adminDTO = createAdminDTO();
+        given(adminRepository.findById(adminDTO.getId())).willReturn(Optional.of(adminDTO.toEntity()));
+        given(categoryRepository.findById(1L)).willReturn(Optional.of(category));
+
+
+        // When
+        categoryService.delete(categoryIds, adminDTO);
+
+        // Then
+        assertThat(category.getDeletedAt()).isNotNull();
+        assertThat(category.getDeletedBy()).isNotNull();
+
+    }
+
+
+    private CategoryDTO createCategoryDTO(Long id, Long parentId) {
         return new CategoryDTO(id,
                 "category_name",
                 parentId,
                 LocalDateTime.now(),
-                1L,
+                any(AdminDTO.class),
                 LocalDateTime.now(),
-                1L,
+                any(AdminDTO.class),
                 null,
-                null);
+                null,
+                Set.of());
     }
 
-    private Category createdCategory(Long id, Long parentId) {
+    private Category createCategory(Long id, Long parentId) {
         return new Category(id,
                 "category_name",
                 parentId,
                 Set.of());
     }
+
+    private AdminDTO createAdminDTO() {
+        return new AdminDTO(1L, "email", "password", "string", "nickname");
+    }
+
+    private Category createCategoryWithChild(Long id, Set<Category> childrens) {
+        return new Category(id,
+                "category_name",
+                null,
+                childrens);
+    }
+
 }
