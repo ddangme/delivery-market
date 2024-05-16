cd179e86a42ce2666e4bdd8c8f272230e8150453
ddangme <ssilver121@naver.com>
2024-05-16 22:19:06 +0900

    docs: 장바구니 DB 에서 good 정보 제외

--- delivery-market/src/main/java/com/ddangme/dm/model/good/Cart.java
+++ delivery-market/src/main/java/com/ddangme/dm/model/good/Cart.java
@@ -1,7 +1,9 @@
 package com.ddangme.dm.model.good;
 
 import com.ddangme.dm.model.member.Member;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 import javax.persistence.*;
@@ -9,6 +11,7 @@ import javax.persistence.*;
 @Getter
 @Entity
 @ToString
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Cart {
 
     @Id
@@ -21,16 +24,26 @@ public class Cart {
     private Member member;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "good_id")
-    @ToString.Exclude
-    private Good good;
-
-    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "option_id")
     @ToString.Exclude
-    private GoodOption goodOption;
+    private GoodOption option;
 
     private Long count;
 
     private Boolean status;
+
+    private Cart(Member member, GoodOption option, Long count) {
+        this.member = member;
+        this.option = option;
+        this.count = count;
+        this.status = true;
+    }
+
+    public static Cart create(Member member, GoodOption option, Long count) {
+        return new Cart(member, option, count);
+    }
+
+    public void addCount(Long count) {
+        this.count += count;
+    }
 }
