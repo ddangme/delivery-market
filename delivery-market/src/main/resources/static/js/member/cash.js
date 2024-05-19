4017a4a95dd29783e0cc4b467e6282a25973e3b4
ddangme <ssilver121@naver.com>
2024-05-20 02:39:19 +0900

    feat: 캐시 충전 리스트 프론트 연결

--- delivery-market/src/main/resources/static/js/member/cash.js
+++ delivery-market/src/main/resources/static/js/member/cash.js
@@ -1,4 +1,65 @@
 $(document).ready(function() {
+    getChargingList();
+    validateAmount();
+    submit();
+});
+
+const trArea = $(`
+<tr>
+    <td class="amount"></td>
+    <td class="status"></td>
+    <td class="requestAt"></td>
+    <td class="responseAt"></td>
+</tr>
+`);
+
+function getChargingList() {
+    $.get(
+        "/api/my-page/cash/list",
+        function (response) {
+            response.forEach(function (item) {
+                console.log(item);
+                let tr = trArea.clone();
+                tr.attr('id', item.id);
+                tr.find('.amount').text(item.amount.toLocaleString() + " 원");
+                tr.find('.status').text(item.status);
+                changeFontColor(item.status, tr.find('.status'));
+                tr.find('.requestAt').text(parseDate(item.requestAt));
+                if (item.responseAt === null) {
+                    tr.find('.responseAt').text("-");
+                } else {
+                    tr.find('.responseAt').text(parseDate(item.responseAt));
+                }
+                $('#cash-list').append(tr);
+            });
+        }
+    );
+}
+
+function changeFontColor(response, $td) {
+    if (response === "요청") {
+        $td.addClass('')
+    } else if (response === "승낙") {
+        $td.addClass('text-success');
+    } else if (response === "거절") {
+        $td.addClass('text-danger')
+    } else if (response === "보류") {
+        $td.addClass('text-warning');
+    }
+}
+
+function parseDate(request) {
+    const date = new Date(request);
+
+    return date.getFullYear() + '년 ' +
+        (date.getMonth() + 1) + '월 ' +
+        date.getDate() + '일 ' +
+        date.getHours().toString().padStart(2, '0') + ':' +
+        date.getMinutes().toString().padStart(2, '0') + ':' +
+        date.getSeconds().toString().padStart(2, '0');
+}
+
+function validateAmount() {
     $('#amount').on('input', function() {
         let inputVal = $(this).val().replace(/[^0-9]/g, '');
 
@@ -7,7 +68,9 @@ $(document).ready(function() {
         }
         $(this).val(inputVal);
     });
+}
 
+function submit() {
     $('#submit').click(function () {
         const amount = $('#amount').val();
         const form = $('#form');
@@ -22,10 +85,11 @@ $(document).ready(function() {
             success: function (response) {
                 alert("충전 요청을 하였습니다.");
                 $('#amount').val('');
+                location.reload();
             },
             error: function (xhr) {
-                alert("충전에 실패하였습니다.\n" + xhr.responseText);
+                alert("충전 요청에 실패하였습니다.\n" + xhr.responseText);
             },
         });
     });
-});
+}
