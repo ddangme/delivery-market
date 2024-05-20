d65796f373f9bfb8849659fa014026579432c7f6
ddangme <ssilver121@naver.com>
2024-05-20 17:26:44 +0900

    feat: 관리자 - 캐시 요청 리스트 페이징 처리

--- delivery-market-admin/src/main/java/com/ddangme/dmadmin/controller/member/MemberManageApiController.java
+++ delivery-market-admin/src/main/java/com/ddangme/dmadmin/controller/member/MemberManageApiController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,7 +21,7 @@ public class MemberManageApiController {
     private final CashManageService cashManageService;
 
     @GetMapping("/cash/list")
-    public ResponseEntity<Page<CashListProjection>> getCashList(Pageable pageable) {
+    public ResponseEntity<Page<CashListProjection>> getCashList(@PageableDefault(size = 2) Pageable pageable) {
         return ResponseEntity.ok(cashManageService.search(pageable));
     }
 }
--- delivery-market-admin/src/main/resources/static/js/member/cash-list.js
+++ delivery-market-admin/src/main/resources/static/js/member/cash-list.js
@@ -1,3 +1,4 @@
+const currentUrl = window.location.href;
 const trArea = $(`
 <tr>
     <td class="id"></td>
@@ -16,32 +17,77 @@ $(document).ready(function () {
 
 function getCashChargingList() {
     $.ajax({
-        url: "/api/members/cash/list",
+        url: '/api/members/cash' + currentUrl.substring(currentUrl.lastIndexOf('/list')),
         type: "GET",
         success: function (response) {
             console.log(response);
-            response.content.forEach(function (data) {
-                let tr = trArea.clone();
-                tr.find('.id').text(data.id);
-                tr.find('.amount').text(data.amount);
-                tr.find('.status').text(data.status);
-                changeFontColor(data.status, tr.find('.status'));
-                tr.find('.memberName').text(data.memberName);
-                tr.find('.adminName').text(data.adminName);
-                tr.find('.requestAt').text(parseDate(data.requestAt));
-                if (data.responseAt === null) {
-                    tr.find('.responseAt').text("-");
-                } else {
-                    tr.find('.responseAt').text(parseDate(data.responseAt));
-                }
+            addData(response.content);
 
-                $('#cash-charging-list').append(tr);
-            })
+            const pageInfo = {
+                totalPages: response.totalPages,
+                totalElements: response.totalElements,
+                pageNumber: response.pageable.pageNumber,
+                pageSize: response.pageable.pageSize,
+                sort: response.pageable.sort
+            }
+
+            addPagination(pageInfo);
         },
         error: function (xhr) {
             alert(xhr.responseText);
             window.history;
         },
+    })
+}
+
+function addPagination(pageInfo) {
+    // 이전 페이지 링크 설정
+    var previousPageLink = $('#pagination .page-item:first-child .page-link');
+    if (pageInfo.pageNumber <= 0) {
+        previousPageLink.addClass('disabled').attr('href', '#');
+    } else {
+        previousPageLink.removeClass('disabled').attr('href', '/members/cash/list?page=' + (pageInfo.pageNumber - 1));
+    }
+
+    // 페이지 번호 링크 설정
+    $('#pagination .page-item:not(:first-child):not(:last-child)').remove(); // 이전 페이지 번호 제거
+    for (var i = 0; i < pageInfo.totalPages; i++) {
+        var pageNumber = i + 1;
+        var pageItem = $('<li class="page-item"></li>');
+        var pageLink = $('<a class="page-link" href="/members/cash/list?page=' + i + '">' + pageNumber + '</a>');
+        if (i === pageInfo.pageNumber) {
+            pageLink.addClass('disabled');
+        }
+        pageItem.append(pageLink);
+        $('#pagination .page-item:last-child').before(pageItem);
+    }
+
+    // 다음 페이지 링크 설정
+    var nextPageLink = $('#pagination .page-item:last-child .page-link');
+    if (pageInfo.pageNumber >= pageInfo.totalPages - 1) {
+        nextPageLink.addClass('disabled').attr('href', '#');
+    } else {
+        nextPageLink.removeClass('disabled').attr('href', '/members/cash/list?page=' + (pageInfo.pageNumber + 1));
+    }
+}
+
+function addData(content) {
+    content.forEach(function (data) {
+        let tr = trArea.clone();
+        tr.find('.id').text(data.id);
+        tr.find('.amount').text(data.amount.toLocaleString() + "원");
+        tr.find('.status').text(data.status);
+        changeFontColor(data.status, tr.find('.status'));
+        tr.find('.memberName').text(data.memberName);
+        tr.find('.adminName').text(data.adminName);
+        tr.find('.requestAt').text(parseDate(data.requestAt));
+        if (data.responseAt === null) {
+            tr.find('.responseAt').text("-");
+        } else {
+            tr.find('.responseAt').text(parseDate(data.responseAt));
+        }
+
+        $('#cash-charging-list').append(tr);
     })
 }
 
