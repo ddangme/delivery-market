c04b2c39caa4f0d297f7eb96062de33a5be6bbc7
ddangme <ssilver121@naver.com>
2024-05-25 19:35:33 +0900

    test: 테스트 파일 생성

---
+++ delivery-market-admin/src/test/java/com/ddangme/dmadmin/service/good/GoodServiceTest.java
@@ -0,0 +1,12 @@
+package com.ddangme.dmadmin.service.good;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+@ExtendWith(MockitoExtension.class)
+class GoodServiceTest {
+
+    @InjectMocks
+    private GoodService goodService;
+
+}
