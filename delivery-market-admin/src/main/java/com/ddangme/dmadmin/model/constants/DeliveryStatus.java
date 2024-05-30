359491e87978466739c60942c24548c7bee3ca2a
ddangme <ssilver121@naver.com>
2024-05-30 10:38:33 +0900

    feat: 배송 상태 추가

--- delivery-market-admin/src/main/java/com/ddangme/dmadmin/model/constants/DeliveryStatus.java
+++ delivery-market-admin/src/main/java/com/ddangme/dmadmin/model/constants/DeliveryStatus.java
@@ -10,6 +10,7 @@ public enum DeliveryStatus {
     SHIPPED("배송 중"),
     CANCELLED("구매 취소"),
     DELIVERED("배송 완료"),
+    RETURN_REQUEST("반품 신청"),
     RETURNED("반품 완료"),
     RETURN_PROCESSED("반품 처리중"),
     ;
