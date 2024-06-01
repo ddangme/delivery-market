e7b6f0b9f228f2afdaf132a0258d8109f227e700
ddangme <ssilver121@naver.com>
2024-06-01 22:14:58 +0900

    feat: 후기 Request 생성

---
+++ delivery-market/src/main/java/com/ddangme/dm/dto/review/ReviewRequest.java
@@ -0,0 +1,15 @@
+package com.ddangme.dm.dto.review;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+public class ReviewRequest {
+
+    private Long optionId;
+    private Long orderGoodId;
+    private BigDecimal rating;
+    private String content;
+
+}
